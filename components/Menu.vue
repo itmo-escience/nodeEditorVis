300f6dbe49a714ace70c46d1f06721b5698427ba
Papakkkarlo <egoriv1802@gmail.com>
2020-03-12 19:26:00 +0300

    init menu

---
+++ components/Menu.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="menu">
+        <div class="d-flex">
+            <div class="cel">Width</div>
+            <div class="cel input"><textarea class="textatea" v-model="width"></textarea></div>
+        </div>
+        <div class="d-flex">
+            <div class="cel">Height</div>
+            <div class="cel input"><textarea class="textatea" v-model="height"></textarea></div>
+        </div>
+        <div class="d-flex">
+            <div class="cel">Type</div>
+            <div class="cel input">
+                <select class="select" v-model="type">
+                    <option value="point" selected>Point</option>
+                    <option value="area">Area</option>
+                    <option value="line">Line</option>
+                </select>
+            </div>
+        </div>
+        <div class="d-flex">
+            <div class="cel">Filter</div>
+            <button class="func" @click="openEditor('filter')"></button>
+        </div>
+    </div>
+</template>
+<script>
+import G2 from '@antv/g2';
+
+export default {
+    props: ['data'],
+    data(){
+        return{
+            width: null,
+            height: null,
+            type: null,
+            chart: null
+        }
+    },
+    methods:{
+        openEditor(name){
+            const state = this.$store.state;
+            if(state.functions[name]){
+                state.editor.fromJSON(this.$store.state.functions[name]);
+            }else{
+                state.editor.fromJSON({
+                    "id": "demo@0.1.0",
+                    "nodes": {
+                        "1": {
+                        "id": 1,
+                        "data": {
+                            "chart": this.chart,
+                            "value": ''
+                        },
+                        "inputs": {},
+                        "outputs": {
+                            "num": {
+                                "connections": []
+                            }
+                        },
+                        "position": [80, 200],
+                        "name": "Input"
+                        }
+                    }
+                });
+            }
+            state.editor.on('process', async () => {
+                const out = editor.toJSON().find(d=>d.name === "Output");
+                console.log(out)
+            })
+
+        }
+    },
+    updated(){
+        if(this.chart){
+            this.chart.destroy()
+            this.chart = null;
+        };
+
+        this.chart = new G2.Chart({
+            container: document.querySelector('#container'),
+            width: +this.width | 600,
+            height: +this.height | 300
+        });
+        this.chart.source(this.data);
+
+        switch(this.type){
+            case 'area':
+                this.chart.area().position('time*pm25');
+                break
+            case 'line':
+                this.chart.line().position('time*pm25');
+                break
+            default:
+                this.chart.point().position('time*pm25');
+                break
+        }
+        this.chart.render();
+    }
+}
+</script>
+<style>
+    .menu{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 500px;
+        height: 100vh;
+    }
+    .d-flex{
+        display: flex;
+    }
+    .cel{
+        padding: 5px;
+    }
+    .input{
+        flex-grow: 1;
+    }
+    .textatea{
+        border: 1px solid #000;
+        resize: none;
+        width: 100%;
+        height: 30px;
+        padding: 5px;
+        border-radius: 4px;
+    }
+    .select{
+        height: 10px;
+        padding: 5px;
+        cursor: pointer;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,51 @@
 <template>
   <div>
+    <Menu :data="Data"/>
     <div class="editor">
       <div id='editor'></div>
     </div>
-    <LineChart
+    <div class="chart">
+        <div id="container"></div>
+    </div>
+    <!--<LineChart
       :data='lineData'
-    />
+    />-->
   </div>
 </template>
 <script>
-  import LineChart from '~/components/LineChart'
-
-  // G2
-  let lineData = [
-    {'time': '3-21', 'pm25': 10}, {'time': '3-21', 'pm25': 20},
-    {'time': '3-22', 'pm25': 15}, {'time': '3-22', 'pm25': 20},
-    {'time': '3-23', 'pm25': 10}, {'time': '3-23', 'pm25': 20},
-    {'time': '3-24', 'pm25': 20}, {'time': '3-24', 'pm25': 30}
-  ]
+  import Menu from '~/components/Menu'
 
   export default {
-    components: {
-      LineChart
-    },
+    components: { Menu },
     data () {
       return {
-        lineData: lineData
+        Data: [
+          {'time': '3-21', 'pm25': 10}, {'time': '3-21', 'pm25': 20},
+          {'time': '3-22', 'pm25': 15}, {'time': '3-22', 'pm25': 20},
+          {'time': '3-23', 'pm25': 10}, {'time': '3-23', 'pm25': 20},
+          {'time': '3-24', 'pm25': 20}, {'time': '3-24', 'pm25': 30}
+        ]
       }
+    },
+    mounted(){
+      this.$store.commit('initRete');
     }
   }
 </script>
 <style>
   .editor{
+    position: fixed;
+    left: 500px;
+    bottom: 0px;
     width: 100%;
     height: 500px;
   }
   #editor{
     height: 500px;
+  }
+  .chart{
+    position: fixed;
+    top: 0px;
+    left: 500px;
   }
 </style>
