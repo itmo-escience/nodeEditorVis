a5cd802498e65cf4b04c7b000af90faabb0e321a
Papakkkarlo <egoriv1802@gmail.com>
2020-04-13 19:52:44 +0300

    RampControl slider

--- components/controls/VueRampControl.vue
+++ components/controls/VueRampControl.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="slider" @mouseover="freezEditor(true)" @mouseout="freezEditor(false)">
-        <div class="color" v-for="(color, index) in value.colors" :key="index" :style="{ width: (value.positions[index+1]*200)-5 +'px' }">
+    <div class="slider" ref="slider" @mouseover="freezEditor(true)" @mouseout="freezEditor(false)">
+        <div class="color" v-for="(color, index) in value.colors" :key="index" :style="{ width: ((value.positions[index+1]-value.positions[index])*200) +'px', zIndex: 10-index }">
             <div>
                 <verte picker="square" :value="color" model="hex" :draggable="false" :showHistory="false" @input="change($event, index)">
                     <svg class="picker" viwebox="0 0 10 10">
-                        <rect :width="(value.positions[index+1]*200)-5" height="10"></rect>
+                        <rect :width="((value.positions[index+1]-value.positions[index])*200)" height="10"></rect>
                     </svg>
                 </verte>
             </div>
             <svg class="circle" :style="{ left: (value.positions[index+1]*200)-10 +'px' }" height="20" viewBox="0 0 10 10" 
                 @mousedown="dragStart" @mouseup="dragEnd"  @mousemove="dragging($event, index)">
-                <circle cx="5" cy="5" r="5"></circle>
+                <circle cx="5" cy="5" r="5" :fill="color"></circle>
             </svg>
             <div :style="{visibility: 'hidden'}">{{render}}</div>
         </div>
@@ -27,26 +27,31 @@
           return {
             drag: false,
             render: false,
+
             value: {
-                colors: [
-                    '#2E8AE6'
-                ],
-                positions: [ 0, .5 ]
+                colors: ['#2E8AE6', '#a3377b'],
+                positions: [ 0, .5, .9 ]
             }
           }
         },
         methods: {
             dragStart(){
+                this.freezEditor(true);
                 this.drag = true;
             },
             dragEnd(){
+                this.freezEditor(false);
                 this.drag = false;
             },
             dragging(e, index){
                 if(this.drag){
-                    console.log(e.layerX)
-                    this.value.positions[index+1] = e.layerX / 200;
+                    const offset = this.$refs.slider.getBoundingClientRect().left;
+                    const position = (e.clientX - offset) / 200;
+                    const right = this.value.positions[index+2] || 1;
+                    const left = this.value.positions[index] || 0;
+                    if(position < right && position > left) this.value.positions[index+1] = position;
                     this.render = !this.render;
+                    this.update();
                 }
             },
             freezEditor(freez){
@@ -55,9 +60,12 @@
             },
             change(color, index){
                 this.value.colors[index] = color;
-                
-                // this.putData(this.ikey, this.value)
-                // this.emitter.trigger('process');
+                this.render = !this.render;
+                this.update();
+            },
+            update(){
+                this.putData(this.ikey, this.value)
+                this.emitter.trigger('process');
             }
         }
     }
@@ -75,7 +83,7 @@
     }
     .circle{
         position: absolute;
-        top: 0px;
+        top: -5px;
     }
     .picker{
         height: 10px;
