aba9cd1012b2db4a9e453efd4194eb064b91fcdf
Papakkkarlo <egoriv1802@gmail.com>
2020-04-15 15:52:53 +0300

    freez editor on over color pallet

--- components/controls/VueColorControl.vue
+++ components/controls/VueColorControl.vue
@@ -4,22 +4,28 @@
       value="#000"
       @input="updateValue"/>
   </div>
-  
 </template>
 <script>
 import { Chrome } from "vue-color";
 export default{
   components: {Chrome},
-  props: ['emitter', 'ikey', 'freez', 'getData', 'putData'],
+  props: ['emitter', 'ikey', 'getData', 'putData'],
+  data(){
+    return{
+      freez: false
+    }
+  },
   methods: {
     freezEditor(freez){
-      this.putData(this.freez, freez);
-      this.emitter.trigger('process');
+      this.freez = freez;
     },
     updateValue(color){
       this.putData(this.ikey, color.hex)
       this.emitter.trigger('process');
     }
+  },
+  mounted(){
+    this.emitter.on('nodetranslate', ()=> !this.freez)
   }
 }
 </script>
