b207346315d697cc7b6eb2bb269343ce6d849d51
Papakkkarlo <egoriv1802@gmail.com>
2020-04-21 19:02:11 +0300

    COVID not valid

--- pages/index.vue
+++ pages/index.vue
@@ -11,23 +11,23 @@
       this.$store.commit('initRete');
       const state = this.$store.state;
 
-      const confirmed = d3.csvParse(await this.$axios.$get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'));
-      const deaths = d3.csvParse(await this.$axios.$get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'));
-      const recovered = d3.csvParse(await this.$axios.$get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'));
-      const COVID = confirmed.map(c=>{
-        const name = `${c['Country/Region']}, ${c['Province/State']}`
-        const d = deaths.find(item=> `${item['Country/Region']}, ${item['Province/State']}` === name);
-        const r = recovered.find(item=> `${item['Country/Region']}, ${item['Province/State']}` === name);
-        return {
-          name: name,
-          lat: +c.Lat,
-          lon: +c.Long,
-          confirmed: Object.values(c).slice(4).reduce((a, b) => +a + +b, 0),
-          deaths: d ? Object.values(d).slice(4).reduce((a, b) => +a + +b, 0) : null,
-          recovered: r ? Object.values(r).slice(4).reduce((a, b) => +a + +b, 0) : null
-        }
-      });
-      state.data['COVID'] = COVID;
+      // const confirmed = d3.csvParse(await this.$axios.$get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'));
+      // const deaths = d3.csvParse(await this.$axios.$get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'));
+      // const recovered = d3.csvParse(await this.$axios.$get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'));
+      // const COVID = confirmed.map(c=>{
+      //   const name = `${c['Country/Region']}, ${c['Province/State']}`
+      //   const d = deaths.find(item=> `${item['Country/Region']}, ${item['Province/State']}` === name);
+      //   const r = recovered.find(item=> `${item['Country/Region']}, ${item['Province/State']}` === name);
+      //   return {
+      //     name: name,
+      //     lat: +c.Lat,
+      //     lon: +c.Long,
+      //     confirmed: Object.values(c).slice(4).reduce((a, b) => +a + +b, 0),
+      //     deaths: d ? Object.values(d).slice(4).reduce((a, b) => +a + +b, 0) : null,
+      //     recovered: r ? Object.values(r).slice(4).reduce((a, b) => +a + +b, 0) : null
+      //   }
+      // });
+      // state.data['COVID'] = COVID;
       state.data['cars.csv'] = d3.csvParse(await this.$axios.$get('/data/cars.csv'));
       state.data['branches.json'] = await this.$axios.$get('/data/branches.json');
       const arcs = await this.$axios.$get('/data/arcs.json')
