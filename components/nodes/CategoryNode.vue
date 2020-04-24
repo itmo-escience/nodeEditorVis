88c0627ddcafbbec7caed6c03fea17f9567fce18
Papakkkarlo <egoriv1802@gmail.com>
2020-04-24 12:09:24 +0300

    Category Node: flex sockets

--- components/nodes/CategoryNode.vue
+++ components/nodes/CategoryNode.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="node" :class="[selected(), node.name] | kebab">
         <!--- hide title <div class="title">{{node.name}}</div> -->
-        <div class="input d-flex" v-for="input in inputs()" :key="input.key">
-            <Socket v-socket:input="input" type="input" :socket="input.socket"></Socket>
-            <div class="input-title" v-show="!input.showControl()">{{input.name}}</div>
-            <div class="input-control" v-show="input.showControl()" v-control="input.control"></div>
-        </div>
-        <!-- Outputs-->
-        <div class="output d-flex" v-for="output in outputs()" :key="output.key">
-            <div class="fields-output-title">{{output.name}}</div>
-            <Socket v-socket:output="output" type="output" :socket="output.socket"></Socket>
+        <div class="d-flex space-between">
+            <div>
+                <div class="input d-flex" v-for="input in inputs()" :key="input.key">
+                    <Socket v-socket:input="input" type="input" :socket="input.socket"></Socket>
+                    <div class="input-title" v-show="!input.showControl()">{{input.name}}</div>
+                    <div class="input-control" v-show="input.showControl()" v-control="input.control"></div>
+                </div>
+            </div>
+            <!-- Outputs-->
+            <div>
+                <div class="output d-flex" v-for="output in outputs()" :key="output.key">
+                    <div class="fields-output-title">{{output.name}}</div>
+                    <Socket v-socket:output="output" type="output" :socket="output.socket"></Socket>
+                </div>
+            </div>
         </div>
 
         <div v-if="node.data.values" class="mrr-20 mrl-20">
