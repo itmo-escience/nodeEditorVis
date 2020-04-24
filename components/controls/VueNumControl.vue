1d7c62f621d722c74fb277d93c6ce89446fbf9a2
Papakkkarlo <egoriv1802@gmail.com>
2020-04-24 13:43:35 +0300

    costume num input

--- components/controls/VueNumControl.vue
+++ components/controls/VueNumControl.vue
@@ -1,9 +1,12 @@
 <template>
-    <input type="number"
+    <div class="d-flex align-center space-between" @mouseover="freezEditor(true)" @mouseleave="freezEditor(false)">
+      <div class="minus" @click="minus"></div>
+      <input type="number"
         :value="value" 
         @input="change($event)"
-        @mouseleave="unFreez"
         :placeholder="placeholder"/>
+      <div class="plus" @click="plus"></div>
+    </div>
 </template>
 <script>
     export default { 
@@ -15,8 +18,10 @@
           }
         },
         methods: {
-          unFreez(){
-            this.freez = false;
+          plus(){ this.value++ },
+          minus(){ this.value-- },
+          freezEditor(f){
+            this.freez = f;
           },
           change(e){
             this.freez = true;
@@ -35,3 +40,24 @@
         }
     }
 </script>
+<style>
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type=number] {
+    -moz-appearance: textfield;
+    text-align: center;
+  }
+  .plus, .minus{ margin: 0 10px; }
+  .plus:after{
+    content: url(~assets/plus.svg);
+    position: relative;
+    top: -2px;
+  }
+  .minus:after{
+    content: url(~assets/minus.svg);
+  }
+</style>
