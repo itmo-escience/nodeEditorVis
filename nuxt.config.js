487fb27e8460570890e1bd38bc154e04773d0b2b
Papakkkarlo <egoriv1802@gmail.com>
2020-06-30 17:35:48 +0300

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'WebWis',
+    title: 'WebVis',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
