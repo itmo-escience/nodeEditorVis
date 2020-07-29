4ef5f98d1fef70f03c1c89adbcd6648832753ba4
Papakkkarlo <egoriv1802@gmail.com>
2020-07-29 11:27:44 +0300

    add textControl

---
+++ components/controls/VueTextControl.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="d-flex">
+            <div class="mrr-20">{{ placeholder }}</div>
+            <input  
+                type="string" 
+                :readonly="readonly"
+                :placeholder="placeholder"
+                v-model="val" 
+                @dblclick.stop="" 
+                @pointerdown.stop="" 
+                @pointermove.stop=""/>
+    </div>
+</template>
+<script>
+    import { csvParse, text } from 'd3';
+
+    export default { 
+        props: ['placeholder', 'readonly', 'emitter', 'ikey', 'node', 'getData', 'putData'],
+        data() {
+          return {
+            val: ''
+          }
+        },
+        updated() {
+            this.node.data[this.ikey] = this.val;
+            this.emitter.trigger('process');
+        }
+    }
+</script>
